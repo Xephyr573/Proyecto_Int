d2d85aaada4dfa586196edfa761075487a0e69ca
wacho7468-lang <wacho7468@gmail.com>
2025-11-04 16:42:11 -0300

    modified src/pages/

---
+++ proyecto-int/src/pages/Ingresar.js
@@ -0,0 +1,8 @@
+export default function Ingresar() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Iniciar sesión</h1>
+      <p>Aquí va tu formulario de ingreso</p>
+    </div>
+  );
+}
