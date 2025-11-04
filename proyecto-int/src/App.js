3eda9cd3936d9c21e4ff3aa91157a4e165e2c9e4
Xephyr <Matimegax@gmail.com>
2025-11-04 17:24:26 -0300

    Django y React en funcionamiento

--- proyecto-int/src/App.js
+++ proyecto-int/src/App.js
@@ -1,7 +1,23 @@
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [message, setMessage] = useState("Cargando...");
+
+  useEffect(() => {
+    //Se hara la peticion a la API de Django
+    axios.get('http://127.0.0.1:8000/api/hello')
+    .then(response => {
+      //response.data contiene el JSON que enviamos desde el Django
+      setMessage(response.data.message + " " + response.data.user);
+    })
+    .catch(error => {
+      console.error("Hubo un error al cargar los datos:",error);
+      setMessage("Error al conectar con la API");
+    });
+  }, []); //El arrary vacio [] significa que esto se ejecutara 1 vez al montar el componente
   return (
     <div className="App">
       <header className="App-header">
