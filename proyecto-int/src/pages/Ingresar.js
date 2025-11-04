fe768bbd07db692eded4e45dd86789c4db0e1573
wacho7468-lang <wacho7468@gmail.com>
2025-11-04 17:19:12 -0300

    modified:   proyecto-int/src/pages/Ingresar.css and js files +add pages homejs,css,ingresar.js,ingresar.css, (modified index.html and css default)

--- proyecto-int/src/pages/Ingresar.js
+++ proyecto-int/src/pages/Ingresar.js
@@ -1,8 +1,41 @@
+import "./Ingresar.css";
+import { useNavigate } from "react-router-dom";
+
 export default function Ingresar() {
+  const navigate = useNavigate();
+
+  const volverAtras = () => {
+    navigate(-1); // Navega a la página anterior
+  };
+
   return (
-    <div style={{ padding: 20 }}>
-      <h1>Iniciar sesión</h1>
-      <p>Aquí va tu formulario de ingreso</p>
+    <div className="login-container">
+      <div className="login-box">
+        <h2>Ingresar</h2>
+
+        <form>
+          <div className="form-group">
+            <label>RUT o Correo</label>
+            <input type="text" placeholder="Ingresa tu RUT o correo" />
+          </div>
+
+          <div className="form-group">
+            <label>Contraseña</label>
+            <input type="password" placeholder="Ingresa tu contraseña" />
+          </div>
+
+          <button className="btn-login" type="submit">
+            Entrar
+          </button>
+        </form>
+
+        <a href="https://digital.inacap.cl/tipo-de-usuario/index.html" className="btn-forgot">¿Olvidaste tu contraseña?</a>
+
+        {/* Botón para volver atrás */}
+        <button className="btn-back" onClick={volverAtras}>
+          Volver atrás
+        </button>
+      </div>
     </div>
   );
 }
