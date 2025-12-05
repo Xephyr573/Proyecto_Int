3ccbdb96acf60140ec0a113974d8d1538354ad24
AlexT1021 <alexander.torres08@inacapmail.cl>
2025-12-05 01:22:11 -0300

    Estoy borrando el cache(? Recen que no se rompa nada

--- backend/api/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import view_prueba, login_view, buscar_estudiante
-
-urlpatterns = [
-    path('status/', view_prueba, name='api_status'),
-    path('login/', login_view, name='api_login'),
-    path('estudiantes/buscar/', buscar_estudiante, name='buscar_estudiante'),
-]
